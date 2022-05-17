cf0e5248bb8425232545087a4060acbfedbe6f65
jehadalhorany <alhoranyjehad@gmail.com>
2022-05-17 22:33:49 +0300

    add filter and function for drop down list

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -2,15 +2,19 @@ import './style.css';
 import React from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import image from "./logoss.jpg"
-
+import { useState, useEffect } from 'react'
 
 export const Navbar = () => {
     const navigate = useNavigate();
-
+    const [value, setValue] = useState('');
     const Search = () => {
-    
+
     }
 
+    const dropdownlist = (e) => {
+        setValue(e.target.value)
+
+    }
 
     return (
         <div className='Navbardiv'>
@@ -20,8 +24,16 @@ export const Navbar = () => {
             <Link to="/Login">Login</Link>
             <Link to="/Dashboard">Dashboard</Link>
             <Link to="/Dashboard/:id">details</Link>
-            <input placeholder='Search' onChange={Search} />
+
+            <input type="text" placeholder='Search' onChange={Search} />
             
+
+            <select onChange={dropdownlist}>
+                <option value="Hybrid">Hybrid</option>
+                <option value="diesel">diesel</option>
+                <option value="petrol">petrol</option>
+                <option value="Electricity">Electricity</option>
+            </select>
 
 
         </div>
